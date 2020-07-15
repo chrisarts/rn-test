1b5dfaec83f694dff783afc663745fec6970ee62
Chris Gutierrez <chrisarts.net@gmail.com>
2020-07-15 03:22:41 -0500

    adding stack router and tab router

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,56 +1,25 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import NewScreen from "../screens/New/New.screen";
-import TopScreen from "../screens/Top/Top.screen";
-import PopularScreen from "../screens/Popular/Popular.screen";
-import HotScreen from "../screens/Hot/Hot.screen";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
+import ArticleScreen from "../screens/Article/Article.screen";
 
-const Tab = createMaterialTopTabNavigator();
+const Stack = createStackNavigator();
 
 const Router = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="New"
-        lazy={true}
-        tabBarOptions={{
-          labelStyle: {
-            fontFamily: "Roboto_400Regular",
-            fontSize: 12
-          },
-          allowFontScaling: true,
-        }}
-      >
-        <Tab.Screen
-          name="New"
-          component={NewScreen}
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={TabNavigator}
           options={{
-            headerShown: false,
+            title: "reddit/r/programming",
+            headerTitleAlign: "center",
           }}
         />
-        <Tab.Screen
-          name="Top"
-          component={TopScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="Popular"
-          component={PopularScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="Hot"
-          component={HotScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Tab.Navigator>
+        <Stack.Screen name="Article" component={ArticleScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/Article/Article.screen.js
+++ src/screens/Article/Article.screen.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import { View, Text } from "react-native";
 
 const ArticleScreen = () => {
   return (
     <View>
-
+      <Text>ARTICLE SCREEN...</Text>
     </View>
-  )
+  );
 };
 
 export default ArticleScreen;
